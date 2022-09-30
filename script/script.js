4fe212bf1a841bfcf6f3aabdeaab9ee9cce62744
ksuxaprokopova <111523946+ksuxaprokopova@users.noreply.github.com>
2022-09-30 20:45:22 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 $(".about_items > div").on("click", function(){
-    $(this).children(".about_icon").toggleClass("about_rotate_icon");
+    $(this).children(".about_items_right").children(".about_icon").toggleClass("about_rotate_icon");
     $(this).parent(".about_items").toggleClass("change_color");
     $(this).next(".about_items_text").slideToggle(300);
 })
