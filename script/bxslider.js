0e08424c8979bdaa2d6c9c0bdc476eac34ead267
ksuxaprokopova <111523946+ksuxaprokopova@users.noreply.github.com>
2022-10-04 20:42:03 +0300

    Add files via upload

--- script/bxslider.js
+++ script/bxslider.js
@@ -2,9 +2,9 @@ $(".carusel").bxSlider({
     mode: "fade",
     controls: false,
     randomStart: true,
-    
+    pager:true,
 })
 
 $(".bx-pager").on("click", function(){
-    $(this).children(".bx-pager-item").toggleClass("bx-pager-item:active")
+    $(this).children(".bx-pager-item").toggleClass("bx-pager-item.active")
 })
